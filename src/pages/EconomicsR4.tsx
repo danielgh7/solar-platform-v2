10910aa1dc51d72f80694103a78b664cba0b9541
danielgh7 <mn7gp5tw7f@privaterelay.appleid.com>
2026-09-10 16:50:44 -0600

    feat(r4): compose complete economics workspace

---
+++ src/pages/EconomicsR4.tsx
@@ -0,0 +1,3 @@
+import {Economics} from './Economics';
+import {R4FinancialCompleteness} from '../components/R4FinancialCompleteness';
+export function EconomicsR4(){return <><Economics/><R4FinancialCompleteness/></>}
